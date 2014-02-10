fcac6f5a4d43a4a2ad48eb02bd0768ff809222f4
82038198 <82038198@MPB26475.correiosnet.int>
2014-02-10 16:25:21 -0300

    readequando

--- src/knearestneighbor/Dwelling.java
+++ src/knearestneighbor/Dwelling.java
@@ -1,11 +1,15 @@
 package knearestneighbor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dwelling {
     
     private Type type;
     private int area;
     private int rooms;
     private double distance;
+    private List<Dwelling> neighbors = new ArrayList<>();
     
     public Dwelling(Type type, int area, int rooms) {
         this.type = type;
@@ -16,6 +20,23 @@ public class Dwelling {
     public Dwelling(int area, int rooms) {
         this.area = area;
         this.rooms = rooms;
+    }
+    
+    void measureDistances(int[][] minMax) {
+        int areaMin = minMax[0][0];
+        int areaMax = minMax[0][1];
+        int roomsMin = minMax[1][0];
+        int roomsMax = minMax[1][1];
+        int roomsRange = roomsMax - roomsMin;
+        int areaRange = areaMax - areaMin;
+        for (Dwelling neighbor : neighbors) {
+            double deltaRooms = neighbor.getRooms() - this.getRooms();
+            deltaRooms = (deltaRooms / roomsRange);
+            double deltaArea = neighbor.getArea() - this.getArea();
+            deltaArea = (deltaArea / areaRange);
+            double dstnc = Math.sqrt((Math.pow(deltaRooms, 2) + Math.pow(deltaArea, 2)));
+            neighbor.setDistance(dstnc);
+        }
     }
     
 
---
+++ src/knearestneighbor/DwellingList.java
@@ -0,0 +1,93 @@
+package knearestneighbor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DwellingList {
+    
+    private int k;
+    private List<Dwelling> dwellings = new ArrayList<>();
+    private int areaMin, areaMax, roomsMin, roomsMax;
+    
+    public DwellingList(int k) {
+        this.k = k;
+        //Dwelling[] eNodes = createTrainingNodes(trainingData);
+        //measureDistances(getMinAndMax(trainingData.split(" ")), eNodes);
+    }
+
+    private void determineUnknown(Dwelling[] uNodes, Dwelling[] nodes) {
+        for (Dwelling uNode : uNodes) {
+            //uNode.setNeighbors(nodes);
+            //double distance = measureDistances
+            //uNode.setDistance();
+        }
+    }
+
+    
+
+    private Dwelling[] createNodes(String trainingData) {
+        String[] ed = trainingData.split(" ");
+        Dwelling[] nodes = new Dwelling[(ed.length / 3)];
+        int t = 2, a = 1, r = 0;
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = new Dwelling(Type.get(ed[t]), Integer.parseInt(ed[a]), Integer.parseInt(ed[r]));
+            t += 3;
+            a += 3;
+            r += 3;
+        }
+        return nodes;
+    }
+
+    private void calculateRanges() {
+        roomsMin = 1000000;
+        roomsMax = 0;
+        areaMin = 1000000;
+        areaMax = 0;
+        int a = 1, r = 0;
+        for (int i = 0; i < dwellings.size(); i++) {
+            if (dwellings.get(a).getArea() < areaMin) {
+                areaMin = dwellings.get(a).getArea();
+            }
+            if (dwellings.get(a).getArea() > areaMax) {
+                areaMax = dwellings.get(a).getArea();
+            }
+
+            if (dwellings.get(r).getRooms() < roomsMin) {
+                roomsMin = dwellings.get(r).getRooms();
+            }
+            if (dwellings.get(r).getRooms() > roomsMax) {
+                roomsMax = dwellings.get(r).getRooms();
+            }
+            if (r <= dwellings.size() - 6) {
+                r += 3;
+            }
+            if (a <= dwellings.size() - 5) {
+                a += 3;
+            }
+        }
+    }
+
+    private final String trainingData = "1 350 apartment "
+            + "2 300 apartment "
+            + "3 300 apartment "
+            + "4 250 apartment "
+            + "4 500 apartment "
+            + "4 400 apartment "
+            + "5 450 apartment "
+            + "7 850 house "
+            + "7 900 house "
+            + "7 1200 house "
+            + "8 1500 house "
+            + "9 1300 house "
+            + "8 1240 house "
+            + "10 1700 house "
+            + "9 1000 house "
+            + "1 800 flat "
+            + "3 900 flat "
+            + "2 700 flat "
+            + "1 900 flat "
+            + "2 1150 flat "
+            + "1 1000 flat "
+            + "2 1200 flat "
+            + "1 1300 flat";
+}
